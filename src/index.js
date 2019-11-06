caac51b2b5d77b6ec2910bd8bc69a5a3e32faf51
jsdex <56501672+jsdex@users.noreply.github.com>
2019-11-06 09:23:37 +0300

    sss

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 const canvas = document.querySelector("#my_canvas"),
   ctx = canvas.getContext("2d");
+
+function DecartToDispalyCoordX(cartX) {
+  return cartX + canvas.width / 2;
+}
+
+function DecartToDispalyCoorY(cartY) {
+  return canvas.height / 2 - cartY;
+}
